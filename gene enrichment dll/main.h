42e586461e5554e7fe706ec58e118c42ebf97e8f
Serena <serena.f.yu@gmail.com>
2017-12-07 23:44:47 +1100

    Edited output
    Increased output distance

--- gene enrichment dll/main.h
+++ gene enrichment dll/main.h
@@ -32,15 +32,15 @@ typedef boost::adjacency_list <boost::vecS, boost::vecS, boost::directedS, boost
 typedef boost::graph_traits<WGraph>::vertex_descriptor Vertex;
 typedef boost::adjacency_list <boost::vecS, boost::vecS, boost::directedS, boost::no_property, boost::no_property, boost::listS> UGraph;
 
-std::unordered_map<std::string, uint32_t> geneid;
+std::unordered_map<std::string, int32_t> geneid;
 std::vector<std::string> idgene;
 const float inf = std::numeric_limits<float>::max();
 
 struct Node
 {
-	uint32_t val;
+	int32_t val;
 	Node* next;
-	Node(uint32_t val, Node* next)
+	Node(int32_t val, Node* next)
 	{
 		this->val = val;
 		this->next = next;
@@ -62,14 +62,14 @@ namespace std
 	{
 		size_t operator()(Node const* node) const
 		{
-			return hash<uint32_t>{}(node->val);
+			return hash<int32_t>{}(node->val);
 		}
 	};
 
 	template<>
-	struct hash<pair<uint32_t, uint32_t>>
+	struct hash<pair<int32_t, int32_t>>
 	{
-		size_t operator()(pair<uint32_t, uint32_t> const& pair) const
+		size_t operator()(pair<int32_t, int32_t> const& pair) const
 		{
 			return (pair.first << 16) | pair.second;
 		}
@@ -83,6 +83,11 @@ struct ComparepNode
 		return node1->val == node2->val;
 	}
 };
+
+inline float distance(float x1, float x2, float y1, float y2)
+{
+	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
 
 /////
 /////
