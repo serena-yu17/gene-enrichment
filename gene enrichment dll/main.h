f562f3241e9361158b8d65690516dea6c93ff881
Serena <serena.f.yu@gmail.com>
2017-12-15 23:12:02 +1100

    Updated hash function
    The hash function for pair<int32_t ,int32_t> used earlier was x<<16 + y,
    because at that time the numbers are guarenteed smaller than 30 000. Now
    since I use Entrenz ID instead of self-increment numbers, it can be up
    to 200 000 000, and shift 16 bit will cause a lot of collision.

--- gene enrichment dll/main.h
+++ gene enrichment dll/main.h
@@ -3,7 +3,6 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
-#include <atomic>
 #include <limits>
 #include <random>
 
@@ -13,62 +12,28 @@
 #include <boost/graph/random_layout.hpp>
 #include <boost/graph/topology.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/functional/hash.hpp>
 
 typedef boost::property<boost::edge_weight_t, float> WeightProperty;
 typedef boost::adjacency_list <boost::vecS, boost::vecS, boost::directedS, boost::no_property, WeightProperty, boost::listS> WGraph;
 typedef boost::graph_traits<WGraph>::vertex_descriptor Vertex;
 typedef boost::adjacency_list <boost::vecS, boost::vecS, boost::directedS, boost::no_property, boost::no_property, boost::listS> UGraph;
+typedef std::unordered_set<std::pair<int32_t, int32_t>, boost::hash<std::pair<int32_t, int32_t>>> PairSet;
 
 constexpr float inf = (std::numeric_limits<float>::max)();
 bool running[10064]{ 0 };
 
-struct Node
-{
-	int32_t val;
-	Node* next;
-	Node(int32_t val, Node* next)
-	{
-		this->val = val;
-		this->next = next;
-	}
-	bool operator==(Node const& other) const
-	{
-		return this->val == other.val;
-	}
-	bool operator!=(Node const& other) const
-	{
-		return this->val != other.val;
-	}
-};
-
 namespace std
 {
-	template<>
-	struct hash<Node*>
-	{
-		size_t operator()(Node const* node) const
-		{
-			return hash<int32_t>{}(node->val);
-		}
-	};
-
 	template<>
 	struct hash<pair<int32_t, int32_t>>
 	{
 		size_t operator()(pair<int32_t, int32_t> const& pair) const
 		{
-			return (pair.first << 16) | pair.second;
+			return (pair.first << 16) | pair.second;				   //
 		}
 	};
 }
-
-struct ComparepNode
-{
-	bool operator()(Node const* node1, Node const* node2) const
-	{
-		return node1->val == node2->val;
-	}
-};
 
 inline float distance(float x1, float x2, float y1, float y2)
 {
